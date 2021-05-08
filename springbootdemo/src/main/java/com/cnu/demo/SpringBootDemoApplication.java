daa2f4af60eb220095296acbc202347ec8de520d
srvakiti <srinureddy360@gmail.com>
2021-05-08 17:09:46 -0400

    updated pom.xml to build as war file

--- springbootdemo/src/main/java/com/cnu/demo/SpringBootDemoApplication.java
+++ springbootdemo/src/main/java/com/cnu/demo/SpringBootDemoApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication 
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//if we package as jar and without 
+//if we package as war ,if we try to run as web application we get whitelable errorpage we need to add component scan
+//@ComponentScan(basePackages= {"com.cnu"})
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
